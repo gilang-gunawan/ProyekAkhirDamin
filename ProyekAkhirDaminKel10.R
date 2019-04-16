5ac4ae10ceae7450b3547917fa918563bc77153a
msnoas <nabilarahmadhani@gmail.com>
2019-04-16 15:10:16 +0700

    Update ProyekAkhirDaminKel10.R
    kasih penjeasa per langkah+nambahin dikit

--- ProyekAkhirDaminKel10.R
+++ ProyekAkhirDaminKel10.R
@@ -1,10 +1,16 @@
+#required package
 library(mice)
-
-data <- read.csv("e:/census-income.csv", header = FALSE, na.strings = " ?")
+#load csv, renamed as data
+data <- read.csv("census-income.csv", header = FALSE, na.strings = " ?")
 data
+
+
 View(data)
 summary(data)
-
+#statistika ringkasan
 str(data$V35)
 summary(data$V36)
-
+#cleaning dataset
+data[data=="?"] <- NA
+#menghilangkan missing value
+data <- na.omit(data)
