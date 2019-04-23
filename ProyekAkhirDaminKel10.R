03c59597425139e5dd9413e030215f661a7cebe9
gilangipb <gilang_gunawan@apps.ipb.ac.id>
2019-04-23 13:07:35 +0700

    hilangin foreign and not citizen

--- ProyekAkhirDaminKel10.R
+++ ProyekAkhirDaminKel10.R
@@ -1,10 +1,18 @@
+#import package
 library(mice)
 
+#load data
 data <- read.csv("e:/census-income.csv", header = FALSE, na.strings = " ?")
-data
 View(data)
 summary(data)
+md.pattern(data)
 
 str(data$V35)
 summary(data$V36)
 
+#menghilangkan instance yang lahir di luar US and not citizen of US
+new_data <- data[data$V36!=" Foreign born- Not a citizen of U S ",]
+
+summary(new_data)
+View(new_data)
+summary(new_data$V42)
