87b60fed5c67737b6b07543bea9ebe4868a22bb3
gilangipb <gilang_gunawan@apps.ipb.ac.id>
2019-04-28 15:03:02 +0700

    ubah manual jadi looping

--- ProyekAkhirDaminKel10.R
+++ ProyekAkhirDaminKel10.R
@@ -1,6 +1,8 @@
 #import package
 library(mice)
 
+## PRAPROSES
+
 #load data
 data <- read.csv("e:/census-income.csv", header = FALSE, na.strings = " ?")
 
@@ -33,94 +35,24 @@ View(data)
 #menghilangkan instance yang lahir di luar US and not citizen of US
 new_data <- data[data$citizenship!=" Foreign born- Not a citizen of U S ",]
 summary(new_data$citizenship)
+new_data$citizenship <- factor(new_data$citizenship)
+summary(new_data$citizenship)
 
 #menghilangkan instance yang berumur kurang dari 14 tahun
 new_data <- new_data[new_data$age>14,]
 summary(new_data$age)
 
+#memberikan nilai string "unknown" pada missing value
 
-#memberikan nilai pada missing value
-summary(new_data)
-View(new_data)
-str(new_data)
+for (i in 1:ncol(new_data)) {
+  if(sum(is.na(new_data[,i]))>0){
+    
+    levels <- levels(new_data[,i])
+    levels[length(levels)+1] <- "unknown"
+    
+    new_data[,i] <- factor(new_data[,i], levels = levels)
+    new_data[,i][is.na(new_data[,i])] <- "unknown"
+  }
+}
 
-#memberi nilai na pada country_of_birth_father menjadi unknown
-summary(new_data$country_of_birth_father) #ada 4085 dara NA
-levels <- levels(new_data$country_of_birth_father)
-levels[length(levels)+1] <- "unknown"
-
-new_data$country_of_birth_father <- factor(new_data$country_of_birth_father, levels = levels)
-new_data$country_of_birth_father[is.na(new_data$country_of_birth_father)] <- "unknown"
-
-summary(new_data$country_of_birth_father)
-
-#memberi nilai na pada country_of_birth_mother menjadi unknown
-summary(new_data$country_of_birth_mother) #ada 4085 dara NA
-levels <- levels(new_data$country_of_birth_mother)
-levels[length(levels)+1] <- "unknown"
-
-new_data$country_of_birth_mother <- factor(new_data$country_of_birth_mother, levels = levels)
-new_data$country_of_birth_mother[is.na(new_data$country_of_birth_mother)] <- "unknown"
-
-summary(new_data$country_of_birth_mother)
-
-#memberi nilai na pada country_of_birth_self menjadi unknown
-summary(new_data$country_of_birth_self) #ada 4085 dara NA
-levels <- levels(new_data$country_of_birth_self)
-levels[length(levels)+1] <- "unknown"
-
-new_data$country_of_birth_self <- factor(new_data$country_of_birth_self, levels = levels)
-new_data$country_of_birth_self[is.na(new_data$country_of_birth_self)] <- "unknown"
-
-summary(new_data$country_of_birth_self)
-
-#mengisi nilai na pada migration_prev_res_in_sunbelt menjadi unknown
-summary(new_data$migration_prev_res_in_sunbelt) #ada 4085 dara NA
-levels <- levels(new_data$migration_prev_res_in_sunbelt)
-levels[length(levels)+1] <- "unknown"
-
-new_data$migration_prev_res_in_sunbelt <- factor(new_data$migration_prev_res_in_sunbelt, levels = levels)
-new_data$migration_prev_res_in_sunbelt[is.na(new_data$migration_prev_res_in_sunbelt)] <- "unknown"
-
-summary(new_data$migration_prev_res_in_sunbelt)
-
-# isi nilai na migration_code-move_within_reg menjadi unknown
-summary(new_data$migration_code_move_within_reg)
-levels <- levels(new_data$migration_code_move_within_reg)
-levels[length(levels)+1] <- "unknown"
-
-new_data$migration_code_move_within_reg <- factor(new_data$migration_code_move_within_reg, levels = levels)
-new_data$migration_code_move_within_reg[is.na(new_data$migration_code_move_within_reg)] <- "unknown"
-
-summary(new_data$migration_code_move_within_reg)
-
-# isi nilai na pada migration_code-change_in_msa menjadi unknown
-summary(new_data$migration_code_change_in_msa)
-levels <- levels(new_data$migration_code_change_in_msa)
-levels[length(levels)+1] <- "unknown"
-
-new_data$migration_code_change_in_msa <- factor(new_data$migration_code_change_in_msa, levels = levels)
-new_data$migration_code_change_in_msa[is.na(new_data$migration_code_change_in_msa)] <- "unknown"
-
-summary(new_data$migration_code_change_in_msa)
-
-# isi nilai na pada migration_code-change_in_reg mnjd unknown
-summary(new_data$migration_code_change_in_reg)
-levels <- levels(new_data$migration_code_change_in_reg)
-levels[length(levels)+1] <- "unknown"
-
-new_data$migration_code_change_in_reg <- factor(new_data$migration_code_change_in_reg, levels = levels)
-new_data$migration_code_change_in_reg[is.na(new_data$migration_code_change_in_reg)] <- "unknown"
-
-summary(new_data$migration_code_change_in_reg)
-
-# isi nilai na pada state_of_previous_residence mnjd unknown
-summary(new_data$state_of_previous_residence)
-levels <- levels(new_data$state_of_previous_residence)
-levels[length(levels)+1] <- "unknown"
-
-new_data$state_of_previous_residence <- factor(new_data$state_of_previous_residence, levels = levels)
-new_data$state_of_previous_residence[is.na(new_data$state_of_previous_residence)] <- "unknown"
-
-summary(new_data$state_of_previous_residence)
 
