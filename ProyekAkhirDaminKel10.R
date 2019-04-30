6b0f7225600794e2b1e2352652508b3ce06c1cd3
gilangipb <gilang_gunawan@apps.ipb.ac.id>
2019-04-30 18:49:50 +0700

    tambahin confussion matrix

--- ProyekAkhirDaminKel10.R
+++ ProyekAkhirDaminKel10.R
@@ -3,16 +3,19 @@ library(party)
 library(mice)
 library(ggplot2)
 library(rpart)
-
-
+library(usdm)
+library(caret)
 ## PRAPROSES
 
 #load data
 data <- read.csv("e:/census-income.csv", header = FALSE, na.strings = " ?")
+
 #eksplorasi
+nrow(data)
 View(data)
 summary(data)
 str(data)
+
 md.pattern(data)
 
 #isi nama atribut/colom
@@ -33,6 +36,8 @@ colnames(data) <- c("age", "class_worker", "det_ind_code",
 
 
 View(data)
+boxplot(weeks_worked ~ citizenship, data = data, col="yellow",xlab="citizenship", ylab = "weeks")
+summary(data$citizenship)
 
 #menghilangkan instance yang lahir di luar US and not citizen of US
 new_data <- data[data$citizenship!=" Foreign born- Not a citizen of U S ",]
@@ -41,14 +46,19 @@ new_data$citizenship <- factor(new_data$citizenship)
 summary(new_data$citizenship)
 
 #menghilangkan instance yang berumur kurang dari 14 tahun
+boxplot(age ~ class_worker, data=data, col="yellow",xlab="class worker", ylab="Age")
+summary(new_data$class_worker)
+hist(data$age)
+
 new_data <- new_data[new_data$age>14,]
 summary(new_data$age)
+summary(new_data)
 
 #memberikan nilai string "unknown" pada missing value
 
 for (i in 1:ncol(new_data)) {
   if(sum(is.na(new_data[,i]))>0){
-    
+    print(names(new_data[i]))
     levels <- levels(new_data[,i])
     levels[length(levels)+1] <- "unknown"
     
@@ -56,18 +66,37 @@ for (i in 1:ncol(new_data)) {
     new_data[,i][is.na(new_data[,i])] <- "unknown"
   }
 }
-View(new_data)
 
+summary(new_data)
+View(new_data)
 
 #bagi data
 set.seed(1234)
-indx <- sample(2, nrow(data), replace=TRUE, prob=c(0.8, 0.2))
+indx <- sample(2, nrow(new_data), replace=TRUE, prob=c(0.8, 0.2))
 train_data <- new_data[indx==1,]
 test_data <- new_data[indx==2,]
 train_label <- factor(train_data[,"class_income"])
 str(train_data)
+summary(train_data)
 
-#test
+summary(test_data$class_income)
+
+#party
+library(party) 
+myFormula <- class_income ~.
+train_ctree <- ctree(myFormula, data=train_data, controls = ctree_control(maxsurrogate = 4))
+print(train_ctree)
+plot(train_ctree)
+plot(train_ctree, type = "simple")
+
+testPred <- predict(train_ctree, newdata = test_data) 
+summary(test_data)
+tabel <- table(testPred, test_data$class_income) 
+tabel
+akurasi <- (tabel[1,1]+tabel[2,2])/(tabel[1,1]+tabel[1,2]+tabel[2,1]+tabel[2,2])
+akurasi*100
+
+#test rpart
 myFormula <- class_income ~.
 train_rpart <- rpart(myFormula, data = train_data, control = rpart.control(minsplit = 100))
 print(train_rpart)
@@ -84,4 +113,6 @@ tabel <- table(test_pred, test_data$class_income)
 tabel
 
 akurasi <- (tabel[1,1]+tabel[2,2])/(tabel[1,1]+tabel[1,2]+tabel[2,1]+tabel[2,2])
-akurasi
+akurasi*100
+
+confusionMatrix(test_pred, test_data$class_income)
